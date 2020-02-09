d81e0b7f2a7b2df7c7a0db22ec830afad6dce921
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-09 03:35:30 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current stat
 
-board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 
 def display_board(board)
   
