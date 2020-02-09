b91347b3317262f6d5100f62ad63456cb87df504
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-09 01:41:50 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current stat
 
+choice = [" ", " ", ]
 def display_board(choice)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
