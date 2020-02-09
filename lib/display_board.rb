e7e248448812d1da51b850056cb2c606ee3f18fa
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-09 03:42:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,20 +2,24 @@
 # out the current stat
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
-board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
-board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
-board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board1 = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board2 = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+board3 = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+board4 = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+board5 = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+board6 = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+board7 = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board8 = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board9 = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-def display_board(board)
-    puts " #{board[0]}  |  #{board[1]} | #{board[2]}  "
+def display_board(input)
+    puts " #{input[0]} |  #{input[1]} | #{input[2]} "
     puts "-----------"
-    puts " #{board[3]}  |  #{board[4]} | #{board[5]}  "
+    puts " #{input[3]} |  #{input[4]} | #{input[5]} "
     puts "-----------"
-    puts "  #{board[6]} | #{board[7]}  | #{board[8]}  "
+    puts " #{input[6]} | #{input[7]}  | #{input[8]} "
 end
+
+display_board(board)
+display_board(board1)
+display_board(board2)
