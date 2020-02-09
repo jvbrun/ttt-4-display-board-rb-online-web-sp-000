e42a0830aed90108401dcd5a3b80a232a0a9d34c
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-09 02:25:46 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,18 +3,9 @@
 
 choice = [" ", " ", " ", " ", "x", " ", " ", " ", " "]
 
-def display_board(choice)
-  
-  row0 = ["#{choice[0]}", "|", "#{choice[1]}", "|", "#{choice[2]}"]
-  row1 = ["-----------"]
-  row2 = ["#{choice[3]}", "|", "#{choice[4]}", "|", "#{choice[5]}"]
-  row3 = ["-----------"]
-  row4 = ["#{choice[6]}", "|", "#{choice[7]}", "|", "#{choice[8]}"]
-  
-  puts row0.inspect
+def display_board(choice[])
+  puts "   |   |   "
   puts "-----------"
-  puts row2.inspect
-  puts "-----------"
-  puts row4.inspect
-  
+  puts "   |   |   "
+  puts "   |   |   "
 end
