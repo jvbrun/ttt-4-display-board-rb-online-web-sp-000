b534c61fd8707976cfa025f25a14251a914146d1
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-09 03:31:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 
-def display_board(board)
+def display_board
     puts " #{board[0]}  |  #{board[1]} | #{board[2]}  "
     puts "-----------"
     puts " #{board[3]}  |  #{board[4]} | #{board[5]}  "
@@ -11,4 +11,4 @@ def display_board(board)
     puts "  #{board[6]} | #{board[7]}  | #{board[8]}  "
 end
 
-puts display_board
+puts display_board(board)
