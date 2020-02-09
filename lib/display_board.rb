b2ebf9b07e51a4333da7d78251944641856d31c7
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-09 03:06:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,46 +4,9 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board)
-  if board = " "
     puts "   |   |   "
     puts "-----------"
     puts "   |   |   "
     puts "-----------"
     puts "   |   |   "
-
-  elsif board[4] == 'X'
-    puts "   |   |   "
-    puts "-----------"
-    puts "   | X |   "
-    puts "-----------"
-    puts "   |   |   "
-
-  elsif board[0] == 'O'
-    puts " O |   |   "
-    puts "-----------"
-    puts "   |   |   "
-    puts "-----------"
-    puts "   |   |   "
-    
-   elsif board[0] == 'O' && board[4] == 'X'
-    puts " O |   |   "
-    puts "-----------"
-    puts "   | X |   "
-    puts "-----------"
-    puts "   |   |   " 
-    
-    elsif board[0] == 'X' && board[1] == 'X' && board[2] == 'X'
-    puts " X | X | X "
-    puts "-----------"
-    puts "   |   |   "
-    puts "-----------"
-    puts "   |   |   " 
-    
-    elsif board[6] == 'O' && board[7] == 'O' && board[8] == 'O'
-    puts "   |   |   "
-    puts "-----------"
-    puts "   |   |   "
-    puts "-----------"
-    puts " O | O | O "
-  end
 end
